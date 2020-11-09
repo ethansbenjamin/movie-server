b934e8cdd94f7ae3928134d6ba091996fdde20ea
Ethan Benjamin <ethanb3njamin@gmail.comn>
2020-11-09 08:38:32 -0500

    added the authentication and user routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,24 @@
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  // this is the sign up routes
+  app.post(
+    "/api/auth/signup",
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted,
+    ],
+    // call the signup method
+    controller.signup
+  );
+  // call the sign in method
+  app.post("/api/auth/signin", controller.signin);
+};
