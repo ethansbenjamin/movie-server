68777ee93be835b718674e161464b0479f2db368
Ethan Benjamin <ethanb3njamin@gmail.comn>
2020-11-09 08:06:10 -0500

    the boiler

--- models/User.model.js
+++ models/User.model.js
@@ -1,26 +1,27 @@
 const mongoose = require("mongoose");
 
-const PostSchema = mongoose.Schema({
-  Name: {
-    type: String,
-    required: true,
+const PostSchema = mongoose.Schema(
+  {
+    Email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+    },
+    Movies: {
+      type: Array,
+      required: false,
+    },
+    Reviews: {
+      type: Array,
+      required: false,
+    },
   },
-  Email: {
-    type: String,
-    required: true,
-  },
-  Password: {
-    type: String,
-    required: true
-},
-  Movies: {
-    type: Array,
-    required: false,
-  },
-  Reviews: {
-    type: Array,
-    required: false,
-  }
-});
+  // the collection the user model will be stored in
+  { collection: "Users" }
+);
 
 module.exports = mongoose.model("User", PostSchema);
