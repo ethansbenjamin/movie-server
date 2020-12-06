321747dff62377ba6ffbe2d201c49e25a08de6e8
Ethan Benjamin <ethanb3njamin@gmail.comn>
2020-12-06 17:26:46 -0500

    added port 3001

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/", (request, response) => {
 });
 // listen to server on this port
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
